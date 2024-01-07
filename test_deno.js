ca01cd97c1e1efcf55aeb8e115ec43f092af0da7
hienld <leduchien8901@gmail.com>
2024-01-08 00:58:42 +0700

    test3

--- test_deno.js
+++ test_deno.js
@@ -1,9 +1,9 @@
 import _ from "https://deno.land/std@0.120.0/node/module.ts";
 
-const httpGet = async (url) => {
-    const data = await fetch(url).then(data => data.json());
-    return data;
-}
+// const httpGet = async (url) => {
+//     const data = await fetch(url).then(data => data.json());
+//     return data;
+// }
 
 /* 
 * @params {string} '["[\"orai1u0vfsjqh0uztlmlwv9cswggn5xkvrt4sayaxme\"]"]'
@@ -15,18 +15,22 @@ const main = async () => {
     // We use contract with code id: 294
     // source contract code: https://github.com/oraichain/oraiwasm/blob/master/package/base/provider/src/helpers.rs
     // In this script we'll only care the about the first element in the array
-    const accountUser = "orai1u0vfsjqh0uztlmlwv9cswggn5xkvrt4sayaxme";
-    const url = `https://lcd.testnet.orai.io/cosmos/bank/v1beta1/balances/${accountUser}`;
-    const result = await httpGet(url);
-    for (let objectStruct of result.balances) {
-        if (objectStruct.denom == "orai") {
-            responses.push({
-                account: accountUser,
-                amounts: [objectStruct.amount]
-            });
-        }
+    // const accountUser = "orai1u0vfsjqh0uztlmlwv9cswggn5xkvrt4sayaxme";
+    // const url = `https://lcd.testnet.orai.io/cosmos/bank/v1beta1/balances/${accountUser}`;
+    // const result = await httpGet(url);
+    // for (let objectStruct of result.balances) {
+    //     if (objectStruct.denom == "orai") {
+    //         responses.push({
+    //             account: accountUser,
+    //             amounts: [objectStruct.amount]
+    //         });
+    //     }
+    // }
+    console.log(JSON.stringify([{
+        account: "orai1u0vfsjqh0uztlmlwv9cswggn5xkvrt4sayaxme",
+        amounts: ["441051731"]
     }
-    console.log(JSON.stringify(responses))
+    ]))
 };
 
 main()
