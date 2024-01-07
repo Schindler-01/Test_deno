5fb7b01bad4ec98ddfbd9082bb184e48b003bbc3
hienld <leduchien8901@gmail.com>
2024-01-07 23:56:07 +0700

    add vector

--- test_deno.js
+++ test_deno.js
@@ -2,8 +2,8 @@ import _ from "https://deno.land/std@0.120.0/node/module.ts";
 
 const main = async () => {
     console.log(JSON.stringify([
-        { id: '14', amount: '4277' },
-        { id: '15', amount: '4277' },
+        { id: '14', amount: ['4277'] },
+        { id: '15', amount: ['4277'] },
     ]))
 };
 
