0d13b1c17c40debb0ac44b75059ed8425d055baf
hienld <leduchien8901@gmail.com>
2024-01-08 00:17:44 +0700

    check account

--- test_deno.js
+++ test_deno.js
@@ -2,8 +2,8 @@ import _ from "https://deno.land/std@0.120.0/node/module.ts";
 
 const main = async () => {
     console.log(JSON.stringify([
-        { id: '14', amount: ['4277'] },
-        { id: '15', amount: ['4277'] },
+        { id: 'oraivaloper18hr8jggl3xnrutfujy2jwpeu0l76azprkxn29v', amount: ['4277'] },
+        { id: 'oraivaloper18hr8jggl3xnrutfujy2jwpeu0l76azprkxn29v', amount: ['4277'] },
     ]))
 };
 
