725fca182e62f3b6039d5b893226ce2addb2543e
hienld <leduchien8901@gmail.com>
2024-02-03 16:25:32 +0700

    test2

--- test3.js
+++ test3.js
@@ -1,7 +1,7 @@
 import _ from "https://deno.land/std@0.120.0/node/module.ts";
 
-const main = async () => {
-    console.log(JSON.stringify(process.env));
+const main = async (symbols) => {
+    console.log(JSON.stringify(symbols));
 }
 
-main()
+main(...process.env)
